1e5d7eb942845f01d0ec01ab35cb1a238f79bdce
Kali machine <yelionel26@gmail.com>
2022-10-25 00:21:15 -0400

    Converting domain name to an IP address

--- portscanner.py
+++ portscanner.py
@@ -3,6 +3,12 @@ import socket
 from IPy import IP
 
 
+def check_ip(ip):
+    try:
+        return IP(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
@@ -14,6 +20,7 @@ def scan_port(ipaddress, port):
 
 
 ipaddress = input('[+] Enter target to scan: ')
+converted_ip = check_ip(ipaddress)
 
 for port in range(75, 85):
-    scan_port(ipaddress, port)
+    scan_port(converted_ip, port)
