e1a1634d441c41b50f1c51ed5bb9b401d554839d
Kali machine <yelionel26@gmail.com>
2022-10-25 00:13:47 -0400

    Adding timeout function

--- portscanner.py
+++ portscanner.py
@@ -2,12 +2,18 @@ import ipaddress
 import socket
 from IPy import IP
 
-ipaddress = input('[+] Enter target to scan: ')
-port = 80
 
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress, port))
-    print('[+] Port 80 is opened')
-except:
-    print('[-] Port 80 is closed')
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print('[+] Port {} is open'.format(port))
+    except:
+        print('[-] Port {} is closed'.format(port))
+
+
+ipaddress = input('[+] Enter target to scan: ')
+
+for port in range(75, 85):
+    scan_port(ipaddress, port)
